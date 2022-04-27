ae58a1a00244f1e768bd2d4353cc30d3e5000ba7
tyakamyz <tyakamyz@naver.com>
2022-04-27 21:50:50 +0900

    feat: 연관관계 매핑 학습을 위한 도메인 생성

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -17,4 +17,29 @@ public class Member {
 
     @Column(name = "TEAM_ID")
     private Long teamId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
 }
