d493eb4584660dcba166c8738cf3f54d446ae469
tyakamyz <tyakamyz@naver.com>
2022-04-27 21:49:44 +0900

    feat: 연관관계 매핑 학습을 위한 도메인 생성

--- src/main/java/jpabook/jpashop/domain/Team.java
+++ src/main/java/jpabook/jpashop/domain/Team.java
@@ -6,15 +6,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Member {
+public class Team {
 
     @Id @GeneratedValue
-    @Column(name = "MEMBER_ID")
+    @Column(name = "TEAM_ID")
     private Long id;
 
-    @Column(name = "USERNAME")
-    private String username;
+    private String name;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
 }
