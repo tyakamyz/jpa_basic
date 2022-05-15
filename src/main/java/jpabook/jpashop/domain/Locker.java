1ed648e20f2f539e85417ae5c5890e48dcf4aa3d
tyakamyz <tyakamyz@naver.com>
2022-05-15 13:33:38 +0900

    feat: 실전 예제4 - 상속관계 매핑 학습

--- src/main/java/jpabook/jpashop/domain/Locker.java
+++ src/main/java/jpabook/jpashop/domain/Locker.java
@@ -3,7 +3,7 @@ package jpabook.jpashop.domain;
 import javax.persistence.*;
 
 @Entity
-public class Locker {
+public class Locker extends BaseEntity {
     @Id @GeneratedValue
     @Column(name = "LOCKER_ID")
     private Long id;
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "ORDER_ITEM_ID")
